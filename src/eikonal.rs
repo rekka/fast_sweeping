89cd530c8c2c2b6d8b11ade5e28f68164a0cb591
Norbert Pozar <bertapozar@gmail.com>
2018-11-08 10:12:41 +0900

    Reordr

--- src/eikonal.rs
+++ src/eikonal.rs
@@ -8,65 +8,6 @@
 //! where ‖.‖ denotes a norm, by performing 2^N Gauss-Seidel sweeps in alternating directions. Here
 //! N is the dimension. Zhao showed that the accuracy for the Euclidean norm is O(h log h).
 
-/// Computes the solution of the eikonal equation in 3D using the fast sweeping algorithm.
-///
-/// `d` should be initialized to a large value at the unknown nodes.
-///
-/// `inv_dual_norm(d, [d1, d2, d3], [s1, s2, s3]) -> t` needs to solve the "inverse problem" for the norm:
-/// Given values `d_i` at points `-s_i e_i`, find the largest value `t ≤ d` at the origin such that
-/// `||p|| ≤ 1`, where `p_i = (s_i (t - d_i))_+` and `||p||` is the __dual__ anisotropic norm.
-pub fn fast_sweep_3d<F>(d: &mut [f64], dim: (usize, usize, usize), mut inv_dual_norm: F)
-where
-    F: FnMut(f64, [f64; 3], [f64; 3]) -> f64,
-{
-    let (nx, ny, nz) = dim;
-    assert_eq!(nx * ny * nz, d.len());
-    let (sx, sy, _sz) = (ny * nz, nz, 1);
-    // see fast_sweep_2d for discussion
-    for p in 1..nx {
-        for dir in 0..2 {
-            let (di_plane, djk, isign) = if dir == 0 {
-                let (low, high) = d.split_at_mut(p * sx);
-                (&low[(p - 1) * sx..][..sx], &mut high[..sx], 1.)
-            } else {
-                let i = nx - 1 - p;
-                let (low, high) = d.split_at_mut((i + 1) * sx);
-                (&high[..sx], &mut low[i * sx..][..sx], -1.)
-            };
-            for q in 1..ny {
-                for dir in 0..2 {
-                    let (di, dj, dk, jsign) = if dir == 0 {
-                        let j = q;
-                        let (low, high) = djk.split_at_mut(j * sy);
-                        (
-                            &di_plane[j * sy..][..sy],
-                            &low[(j - 1) * sy..][..sy],
-                            &mut high[..sy],
-                            1.,
-                        )
-                    } else {
-                        let j = ny - 1 - q;
-                        let (low, high) = djk.split_at_mut((j + 1) * sy);
-                        (
-                            &di_plane[j * sy..][..sy],
-                            &high[..sy],
-                            &mut low[j * sy..][..sy],
-                            -1.,
-                        )
-                    };
-                    for r in 1..nz {
-                        let k = r;
-                        dk[k] = inv_dual_norm(dk[k], [di[k], dj[k], dk[k - 1]], [isign, jsign, 1.]);
-                        let k = nz - 1 - r;
-                        dk[k] =
-                            inv_dual_norm(dk[k], [di[k], dj[k], dk[k + 1]], [isign, jsign, -1.]);
-                    }
-                }
-            }
-        }
-    }
-}
-
 /// Computes the solution of the eikonal equation in 2D using the fast sweeping algorithm.
 ///
 /// `inv_dual_norm(d, [d1, d2], [s1, s2]) -> t` needs to solve the "inverse problem" for the norm:
@@ -142,6 +83,65 @@ where
             dj[j] = inv_dual_norm(dj[j], [di[j], dj[j - 1]], [-1., 1.]);
             let j = ny - 1 - q;
             dj[j] = inv_dual_norm(dj[j], [di[j], dj[j + 1]], [-1., -1.]);
+        }
+    }
+}
+
+/// Computes the solution of the eikonal equation in 3D using the fast sweeping algorithm.
+///
+/// `d` should be initialized to a large value at the unknown nodes.
+///
+/// `inv_dual_norm(d, [d1, d2, d3], [s1, s2, s3]) -> t` needs to solve the "inverse problem" for the norm:
+/// Given values `d_i` at points `-s_i e_i`, find the largest value `t ≤ d` at the origin such that
+/// `||p|| ≤ 1`, where `p_i = (s_i (t - d_i))_+` and `||p||` is the __dual__ anisotropic norm.
+pub fn fast_sweep_3d<F>(d: &mut [f64], dim: (usize, usize, usize), mut inv_dual_norm: F)
+where
+    F: FnMut(f64, [f64; 3], [f64; 3]) -> f64,
+{
+    let (nx, ny, nz) = dim;
+    assert_eq!(nx * ny * nz, d.len());
+    let (sx, sy, _sz) = (ny * nz, nz, 1);
+    // see fast_sweep_2d for discussion
+    for p in 1..nx {
+        for dir in 0..2 {
+            let (di_plane, djk, isign) = if dir == 0 {
+                let (low, high) = d.split_at_mut(p * sx);
+                (&low[(p - 1) * sx..][..sx], &mut high[..sx], 1.)
+            } else {
+                let i = nx - 1 - p;
+                let (low, high) = d.split_at_mut((i + 1) * sx);
+                (&high[..sx], &mut low[i * sx..][..sx], -1.)
+            };
+            for q in 1..ny {
+                for dir in 0..2 {
+                    let (di, dj, dk, jsign) = if dir == 0 {
+                        let j = q;
+                        let (low, high) = djk.split_at_mut(j * sy);
+                        (
+                            &di_plane[j * sy..][..sy],
+                            &low[(j - 1) * sy..][..sy],
+                            &mut high[..sy],
+                            1.,
+                        )
+                    } else {
+                        let j = ny - 1 - q;
+                        let (low, high) = djk.split_at_mut((j + 1) * sy);
+                        (
+                            &di_plane[j * sy..][..sy],
+                            &high[..sy],
+                            &mut low[j * sy..][..sy],
+                            -1.,
+                        )
+                    };
+                    for r in 1..nz {
+                        let k = r;
+                        dk[k] = inv_dual_norm(dk[k], [di[k], dj[k], dk[k - 1]], [isign, jsign, 1.]);
+                        let k = nz - 1 - r;
+                        dk[k] =
+                            inv_dual_norm(dk[k], [di[k], dj[k], dk[k + 1]], [isign, jsign, -1.]);
+                    }
+                }
+            }
         }
     }
 }
